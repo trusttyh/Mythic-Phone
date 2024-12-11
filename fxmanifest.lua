e8f6c8a72f5503f2693a66c21878bd132aaa714a
Tyh <101424789+trusttyh@users.noreply.github.com>
2024-12-11 02:42:30 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,12 +2,12 @@ name 'ARP Phone'
 description 'Phone Written For ARP'
 author '[Alzar]'
 version 'v1.0.0'
-url 'https://www.eternalrp.com'
+url 'https://www.mythicrp.com'
 lua54 'yes'
 fx_version "cerulean"
 game "gta5"
-client_script "@eternal-base/components/cl_error.lua"
-client_script "@eternal-pwnzor/client/check.lua"
+client_script "@mythic-base/components/cl_error.lua"
+client_script "@mythic-pwnzor/client/check.lua"
 
 ui_page 'ui/dist/index.html'
 
